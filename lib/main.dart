c03955b689087ee04bd5d1cb8a72054ef2c608fc
Leonardo Toledo <2025010842@unifei.edu.br>
2025-05-19 23:46:17 -0300

    Criação do AppBar e Body

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 
 class PerguntaApp extends StatelessWidget{
 
+  @override
   Widget build(BuildContext context){
     return MaterialApp(
-      home: Text("Olá Flutter!")
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Programa AsiPerguntas"),
+        ),
+        body: Text("Protótipo do AsiPerguntas") ,
+      )
     );
   }
 
